7cc28de7b8bbf454c8a2d5809d337b48b7dc3d6d
Raymer <Raymer (Trabajos)@RAYMER-PC>
2022-08-03 14:45:50 -0600

    listar cliente

--- ProyectoFinal/src/visual/ListarCliente.java
+++ ProyectoFinal/src/visual/ListarCliente.java
@@ -2,15 +2,27 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import logico.Altice;
+import logico.Trabajador;
+
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class ListarCliente extends JFrame {
 
 	private JPanel contentPane;
+	private JTable tabla;
+	private JScrollPane sp;
+	DefaultTableModel modelo;
 
 	/**
 	 * Launch the application.
@@ -42,17 +54,62 @@ public class ListarCliente extends JFrame {
 		contentPane.setLayout(null);
 		setLocationRelativeTo(null);
 		
-		JButton btnNewButton = new JButton("Listar");
-		btnNewButton.setBounds(208, 352, 89, 23);
+		JButton btnNewButton = new JButton("Agregar");
+		btnNewButton.setBounds(20, 22, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Eliminar");
-		btnNewButton_1.setBounds(10, 352, 89, 23);
+		btnNewButton_1.setBounds(218, 22, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Modificar");
-		btnNewButton_2.setBounds(109, 352, 89, 23);
+		btnNewButton_2.setBounds(119, 22, 89, 23);
 		contentPane.add(btnNewButton_2);
+		
+		sp = new JScrollPane();
+		sp.setBounds(20, 60, 625, 318);
+		contentPane.add(sp);
+		
+		tabla = new JTable();
+		sp.setColumnHeaderView(tabla);
+		obtenerClientes();
+		sp.setViewportView(tabla); 
+		
+	}
+
+	private void obtenerClientes() {
+		modelo = new DefaultTableModel();
+		
+		List<Object> nombreColumna = new ArrayList<Object>();
+		
+		nombreColumna.add("Cedula");
+		nombreColumna.add("Nombre");
+		nombreColumna.add("Apellido");
+		nombreColumna.add("Telefono");
+		nombreColumna.add("Direccion");
+		
+		for(Object columna : nombreColumna) {
+			modelo.addColumn(columna);
+		}
+		
+//		List<Trabajador> usuarios = Altice.getInstance().obtenerTrabajador();
+
+//		Object[] row = new Object[7];
+//		for(Trabajador t : usuarios) {
+//			System.out.print(t.getNombre());
+//
+//			row[0]=t.getNombre();
+//			row[1]=t.getApellidos();
+//			row[2]=t.getCedula();
+//			row[3]=t.getEmail();
+//			row[4]=t.getTelefono();
+//			row[5]=t.getUserName();
+//			row[6]=t.getPassword();
+//			modelo.addRow(row);
+//		}
+		
+		
+		tabla.setModel(modelo);		
 	}
 
 }
